5cf1862663e2afaf1e324cd467fbdf11d53c8966
FellpsDev <felipeffjcontato@gmail.com>
2022-11-05 12:58:43 -0300

    add index.html

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function createGame(team1, hour, team2) {
   `
 }
 
-let delay = - 0.3
-function createCard(date, day, games) {
-  delay += 0.3;
+let delay = - 0.2
+function createCard(date, day, group, games) {
+  delay += 0.2;
   return `
     <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
@@ -22,8 +22,6 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-    createCard("24/11", "sabado", createGame('switzerland', '07:00', 'cameroon') + createGame('portugal', '13:00', 'ghana') + createGame("brazil", "16:00", "serbia")) +
-    createCard("28/11", "quarta", createGame('south_korea', '10:00', 'ghana') + createGame("brazil", "13:00", "switzerland") + createGame('portugal', '16:00', 'uruguay')) +
-    createCard("02/12", "segunda", createGame("cameroon", "16:00", "brazil"))
-
-
+    createCard("24/11", "Sábado", "Grupo G", createGame('switzerland', '07:00', 'cameroon') + createGame("brazil", "16:00", "serbia")) +
+    createCard("28/11", "Quarta", "Grupo G", createGame('cameroon', '07:00', 'serbia') + createGame("brazil", "13:00", "switzerland")) +
+    createCard("02/12", "Segunda", "Grupo G", createGame('serbia', '16:00', 'switzerland') + createGame("cameroon", "16:00", "brazil"))
